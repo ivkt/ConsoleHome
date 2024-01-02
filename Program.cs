02daa3fb9238f47d4df83cd7a6baaa83bb7719b2
ivkt <ivkt@yandex.ru>
2024-01-02 21:17:17 +0300

    ДЗ к уроку 1-6

--- Program.cs
+++ Program.cs
@@ -12,9 +12,17 @@ namespace ConsoleHome
         static void Main(string[] args)
         {
             Position position = new Position();
+            position.PriceChangeEvents += PriceLine;//Добавляем обработчик для события PriceChangeEvents
             Console.ReadLine();
         }
+        //при вызове события PriceChangeEvents будет вызываться этот метод PriceLine,
+        //которому для параметра message будет передаваться строка, которая передается в PriceChangeEvents?.Invoke()
 
+        public static void PriceLine(string message)
+        { 
+            Console.WriteLine($"Позиция изменилась. {message}");
+
+        }
 
     }
 }
